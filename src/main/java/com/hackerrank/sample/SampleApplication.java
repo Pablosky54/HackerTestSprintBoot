7cedcd7c6d8177af97e357f1994624aed0b64e72
Admin <admin.push@hackerrank.workspace>
2026-01-18 15:33:44 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- src/main/java/com/hackerrank/sample/SampleApplication.java
+++ src/main/java/com/hackerrank/sample/SampleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootTest
 public class SampleApplication {
 
     public static void main(String[] args) {
