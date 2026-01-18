04679e343f5c94df9d424ffaeb52659f68d727cc
User <user@hackerrank.workspace>
2026-01-18 15:19:28 +0000

    Add initial repository
    Commit by HackerRank

---
+++ src/main/java/com/hackerrank/sample/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.hackerrank.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Backend is working";
+    }
+}
